4f4cfb63768f27eaee79003374ac122584222b60
Sahan Punchihewa <72688889+SahanPunchihewa@users.noreply.github.com>
2021-12-07 15:21:02 +0530

    Database Connection

--- WindowsFormsApp1/student.cs
+++ WindowsFormsApp1/student.cs
@@ -32,6 +32,24 @@ namespace WindowsFormsApp1
         {
             // Insert Code goes here
 
+            try {
+
+                string MyConnection = "server=localhost; userId=root; password=; database=esoftstudent";
+
+                string query = "INSERT INTO `student` (`id`, `name`, `address`, `gender`, `course`, `phone`) VALUES ('" + this.txt_id.Text + "', '" + this.txt_name.Text + "', '" + this.txt_address.Text + "', '" + this.txt_gender.Text + "', '" + this.txt_course.Text + "', '" + this.txt_phone.Text + "' );";
+                MySqlConnection Myconn = new MySqlConnection(MyConnection);
+                MySqlCommand Mycommand = new MySqlCommand(query, Myconn);
+                MySqlDataReader MyReader;
+                Myconn.Open();
+                MyReader = Mycommand.ExecuteReader();
+                MessageBox.Show("Registration Sucessful", "Registration", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
 
 
         }
